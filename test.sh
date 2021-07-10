4cebf27b654836097b3331327058b4be3c4b5aa3
Fable67 <jonasstepanik@web.de>
2021-07-10 15:14:08 +0200

    Smaller batchsize

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ count=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
 echo 'start'
 for (( c=$count; c>=1; c-- ))
 do
-      python benchmark_models.py -g $c&& &>/dev/null
+      python benchmark_models.py -b 8 -g $c&& &>/dev/null
 done
 echo 'end'
